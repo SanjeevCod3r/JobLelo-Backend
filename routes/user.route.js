e7c385a1a5463ce2220a0679b898b24f7e75b06c
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2024-07-22 14:42:59 +0530

    Testing Left

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { login, register } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/profile/update").post(updateProfile);
+router.route("/profile/update").post(isAuthenticated,updateProfile);
+
+export default router;
 
 
 
