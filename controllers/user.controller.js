882d2b3179b314be6eda4374c58341fe60644456
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2024-07-22 13:53:30 +0530

    Login Logic Done Now Going to Start Logout Logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,35 +1,104 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // Register Logic --->
 
-export const register = async (req, res) =>{
-    try {
-        const {fullname, email, phoneNumber, password, role} = req.body;
-        if(!fullname || !email || !phoneNumber || !password || !role){
-            return res.status(400).json({
-                message:"Something is missing",
-                success:false
-            });
-        };
-        const user = await User.findOne({email});
-        if(user){
-            return res.status(400).json({
-                message:"User already exists",
-                success:false
-            })
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+        success: false,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        await User.create({
-            fullname,
-            email,
-            phoneNumber,
-            password:hashedPassword,
-            role,   
-        })
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+    return res.status(201).json({
+      message: "User created successfully",
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Login Logic --->
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
     }
-    catch (err) {
-        console.log(err);
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "User does not exist",
+        success: false,
+      });
     }
-}
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Password is incorrect",
+        success: false,
+      });
+    }
+
+    // check role is correct or not
+    if (role != user.role) {
+      return res.status(400).json({
+        message: "Role is incorrect",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: User.role,
+      profile: user.profile,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome Back ${user.fullname}`,
+        success: true,
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
