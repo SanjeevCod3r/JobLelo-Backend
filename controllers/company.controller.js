c8c327de449961c8fdbeecc61491086d0ba4214e
SanjeevCod3r <Sanjeev1822.be21@chitkara.edu.in>
2024-07-24 17:09:24 +0530

    routes part done now testing

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -62,9 +62,10 @@ export const updateCompanyInformation = async (req, res) => {
     try {
         const { name, description, website, location } = req.body;
         const file = req.file; 
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-        const logo = cloudResponse.secure_url;
+
+        // idr cloudinary aayega
+
+        
         const updateData = { name, description, website, location , logo};
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
